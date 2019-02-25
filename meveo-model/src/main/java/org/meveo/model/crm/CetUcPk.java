b3aa3a2a286ce6f76dd160bf242741d7d93412ba
clement.bareth <clement.bareth@web-drone.fr>
2019-02-25 18:00:23 +0100

    Refs #3500 : Unique constraint pkey base on code + cet_id
    Signed-off-by: clement.bareth <clement.bareth@web-drone.fr>

---
+++ meveo-model/src/main/java/org/meveo/model/crm/CetUcPk.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2018-2019 Webdrone SAS (https://www.webdrone.fr/) and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. This program is
+ * not suitable for any direct or indirect application in MILITARY industry See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.meveo.model.crm;
+
+import org.meveo.model.customEntities.CustomEntityTemplate;
+
+import java.io.Serializable;
+
+public class CetUcPk implements Serializable {
+
+    private String code;
+    private CustomEntityTemplate customEntityTemplate;
+
+    public CetUcPk(String code, CustomEntityTemplate customEntityTemplate) {
+        this.code = code;
+        this.customEntityTemplate = customEntityTemplate;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public CustomEntityTemplate getCustomEntityTemplate() {
+        return customEntityTemplate;
+    }
+
+    public void setCustomEntityTemplate(CustomEntityTemplate customEntityTemplate) {
+        this.customEntityTemplate = customEntityTemplate;
+    }
+}
