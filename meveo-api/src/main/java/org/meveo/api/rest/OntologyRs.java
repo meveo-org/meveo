1c2c7ded8e79eb65365aff1a3b08f3eef2f11f59
clement.bareth <clement.bareth@web-drone.fr>
2019-03-15 15:34:00 +0100

    Add 1 day cache for ontology schema
    Signed-off-by: clement.bareth <clement.bareth@web-drone.fr>

--- meveo-api/src/main/java/org/meveo/api/rest/OntologyRs.java
+++ meveo-api/src/main/java/org/meveo/api/rest/OntologyRs.java
@@ -5,7 +5,9 @@ import org.meveo.service.crm.impl.JSONSchemaGenerator;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/ontology")
 @Produces({MediaType.APPLICATION_JSON})
@@ -15,7 +17,16 @@ public class OntologyRs extends BaseRs {
     private JSONSchemaGenerator jsonSchemaGenerator;
 
     @GET
-    public String getSchema(@DefaultValue("true") @QueryParam("onlyActivated") boolean onlyActivated){
-        return jsonSchemaGenerator.generateSchema("ontology", onlyActivated);
+    public Response getSchema(@DefaultValue("true") @QueryParam("onlyActivated") boolean onlyActivated){
+        final String ontology = jsonSchemaGenerator.generateSchema("ontology", onlyActivated);
+
+        CacheControl cacheControl = new CacheControl();
+        cacheControl.setMaxAge(86400);
+        cacheControl.setPrivate(true);
+
+        Response.ResponseBuilder builder = Response.ok(ontology);
+        builder.cacheControl(cacheControl);
+
+        return builder.build();
     }
 }
