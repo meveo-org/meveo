d08351785e8572dd0ee6b37f786c07c93964d159
hien.bach <hien.bach@manaty.net>
2019-03-27 19:31:29 +0700

    re : update #3481 - TechnicalServiceEndpoint - UI

--- meveo-admin/web/src/main/java/org/meveo/admin/action/admin/custom/EndpointBean.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/admin/custom/EndpointBean.java
@@ -1,8 +1,14 @@
 package org.meveo.admin.action.admin.custom;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.meveo.admin.action.BaseBean;
+import org.meveo.admin.exception.BusinessException;
+import org.meveo.elresolver.ELException;
+import org.meveo.model.scripts.FunctionIO;
 import org.meveo.model.technicalservice.endpoint.Endpoint;
+import org.meveo.model.technicalservice.endpoint.EndpointParameter;
 import org.meveo.model.technicalservice.endpoint.EndpointPathParameter;
+import org.meveo.model.technicalservice.endpoint.TSParameterMapping;
 import org.meveo.service.base.local.IPersistenceService;
 import org.meveo.service.technicalservice.endpoint.EndpointService;
 import org.primefaces.model.DualListModel;
@@ -25,7 +31,9 @@ public class EndpointBean extends BaseBean<Endpoint> {
     @Inject
     private EndpointService endpointService;
 
-    private DualListModel<EndpointPathParameter> pathParameterListModel;
+    private DualListModel<EndpointPathParameter> pathParametersDL;
+
+    private List<TSParameterMapping> parameterMappings = new ArrayList<>();
 
     /**
      * Constructor. Invokes super constructor and provides class type of this
@@ -55,24 +63,74 @@ public class EndpointBean extends BaseBean<Endpoint> {
         return endpointService;
     }
 
-
-    public DualListModel<EndpointPathParameter> getEndpointPathParameter() {
-
-        if (pathParameterListModel == null) {
+    public DualListModel<EndpointPathParameter> getDualListModel() {
+        if (pathParametersDL == null) {
             List<EndpointPathParameter> perksSource = new ArrayList<>();
-            perksSource =  endpointService.listActive().get(0).getPathParameters();
-            List<EndpointPathParameter> perksTarget = new ArrayList<>();
-            if (getEntity().getPathParameters() != null) {
-                perksTarget.addAll(getEntity().getPathParameters());
+            if (entity.getService() != null && CollectionUtils.isNotEmpty(entity.getService().getInputs())) {
+                List<FunctionIO> functionIOList = entity.getService().getInputs();
+                EndpointPathParameter endpointPathParameter;
+                EndpointParameter endpointParameter;
+                for (FunctionIO functionIO : functionIOList) {
+                    endpointPathParameter = new EndpointPathParameter();
+                    endpointParameter = new EndpointParameter();
+                    endpointParameter.setEndpoint(entity);
+                    endpointParameter.setParameter(functionIO.getName());
+                    endpointPathParameter.setEndpointParameter(endpointParameter);
+                    perksSource.add(endpointPathParameter);
+                }
+                List<EndpointPathParameter> perksTarget = new ArrayList<>();
+                if (getEntity().getPathParameters() != null) {
+                    perksTarget.addAll(getEntity().getPathParameters());
+                }
+                perksSource.removeAll(perksTarget);
+                pathParametersDL = new DualListModel<EndpointPathParameter>(perksSource, perksTarget);
+            } else {
+                List<EndpointPathParameter> perksTarget = new ArrayList<>();
+                pathParametersDL = new DualListModel<EndpointPathParameter>(perksSource, perksTarget);
             }
-            perksSource.removeAll(perksTarget);
-            pathParameterListModel = new DualListModel<EndpointPathParameter>(perksSource, perksTarget);
         }
-        return pathParameterListModel;
+        return pathParametersDL;
     }
 
-    public void setPathParameterListModel(DualListModel<EndpointPathParameter> pathParameterListModel) {
-        this.pathParameterListModel = pathParameterListModel;
+    public void setDualListModel(DualListModel<EndpointPathParameter> pathParametersDL) {
+        this.pathParametersDL = pathParametersDL;
+    }
+
+    public List<TSParameterMapping> getParameterMappings() {
+        List<String> parameterNames = new ArrayList<>();
+        if (pathParametersDL != null && pathParametersDL.getTarget() != null) {
+            TSParameterMapping tsParameterMapping;
+            EndpointParameter endpointParameter;
+            if (getEntity().getParametersMapping() != null) {
+                getEntity().getParametersMapping().forEach(item->parameterNames.add(item.getEndpointParameter().getParameter()));
+                parameterMappings.addAll(getEntity().getParametersMapping());
+            }
+            for (EndpointPathParameter endpointPathParameter : pathParametersDL.getTarget()) {
+                if (!parameterNames.contains(endpointPathParameter.getEndpointParameter().getParameter())) {
+                    tsParameterMapping = new TSParameterMapping();
+                    endpointParameter = new EndpointParameter();
+                    endpointParameter.setEndpoint(entity);
+                    endpointParameter.setParameter(endpointPathParameter.getEndpointParameter().getParameter());
+                    parameterMappings.add(tsParameterMapping);
+                    parameterNames.add(endpointPathParameter.getEndpointParameter().getParameter());
+                }
+            }
+        }
+        return parameterMappings;
+    }
+
+    @Override
+    public String saveOrUpdate(boolean killConversation) throws BusinessException, ELException {
+        log.info("functionIOList size {}", entity.getService().getInputs().size());
+        if (CollectionUtils.isNotEmpty(parameterMappings)) {
+            getEntity().getParametersMapping().clear();
+            getEntity().getParametersMapping().addAll(parameterMappings);
+        }
+        if (pathParametersDL != null && CollectionUtils.isNotEmpty(pathParametersDL.getTarget())) {
+            getEntity().getPathParameters().clear();
+            getEntity().getPathParameters().addAll(pathParametersDL.getTarget());
+        }
+        return super.saveOrUpdate(killConversation);
     }
 
     @Override
