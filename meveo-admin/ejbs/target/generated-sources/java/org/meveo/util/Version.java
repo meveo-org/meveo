3c856ed20b75e5fd5742365e8d281ff12c439031
clement.bareth <clement.bareth@web-drone.fr>
2019-06-10 11:20:21 +0200

    Meveo 6.1.0
    Signed-off-by: clement.bareth <clement.bareth@web-drone.fr>

---
+++ meveo-admin/ejbs/target/generated-sources/java/org/meveo/util/Version.java
@@ -0,0 +1,13 @@
+package org.meveo.util;
+
+public final class Version {
+
+    public static final String build_time="2019-06-06T11:49:50Z";
+
+    public static final String appVersion="6.1.0";
+    
+    public static final String buildNumber="e509e60608ef67786aa820d208e36ab4b5dbc21b";
+    
+    
+
+}
