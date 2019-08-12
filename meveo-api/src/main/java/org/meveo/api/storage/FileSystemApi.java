91a432def931ccef243ca00d990479e5ff2855e9
clement.bareth <clement.bareth@web-drone.fr>
2019-08-12 12:11:52 +0200

    Refs #3810 : FileSytemApi - Use cross storage
    Also :
    - Handled list provisionally
    - Send 404 erorr when binary is empty
    
    Signed-off-by: clement.bareth <clement.bareth@web-drone.fr>

--- meveo-api/src/main/java/org/meveo/api/storage/FileSystemApi.java
+++ meveo-api/src/main/java/org/meveo/api/storage/FileSystemApi.java
@@ -1,58 +1,85 @@
 package org.meveo.api.storage;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
+import org.apache.commons.io.IOUtils;
+import org.meveo.api.BaseApi;
+import org.meveo.api.exception.BusinessApiException;
+import org.meveo.cache.CustomFieldsCacheContainerProvider;
+import org.meveo.exceptions.EntityDoesNotExistsException;
+import org.meveo.model.crm.CustomFieldTemplate;
+import org.meveo.model.customEntities.CustomEntityTemplate;
+import org.meveo.model.storage.Repository;
+import org.meveo.persistence.CrossStorageService;
+import org.meveo.service.storage.BinaryStoragePathParam;
+import org.meveo.service.storage.FileSystemService;
+import org.meveo.service.storage.RepositoryService;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
-import org.meveo.api.BaseApi;
-import org.meveo.api.exception.BusinessApiException;
-import org.meveo.exceptions.EntityDoesNotExistsException;
-import org.meveo.model.crm.CustomFieldTemplate;
-import org.meveo.model.customEntities.CustomEntityInstance;
-import org.meveo.service.crm.impl.CustomFieldInstanceService;
-import org.meveo.service.crm.impl.CustomFieldTemplateService;
-import org.meveo.service.custom.CustomEntityInstanceService;
-import org.meveo.service.storage.BinaryStoragePathParam;
-import org.meveo.service.storage.FileSystemService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * @author Edward P. Legaspi <czetsuya@gmail.com>
+ * @author clement.bareth
  */
 @Stateless
 public class FileSystemApi extends BaseApi {
+
 	@Inject
 	private FileSystemService fileSystemService;
 
 	@Inject
-	private CustomFieldTemplateService cftService;
+	private CustomFieldsCacheContainerProvider cache;
 
 	@Inject
-	private CustomFieldInstanceService cfiService;
+	private RepositoryService repositoryService;
 
 	@Inject
-	private CustomEntityInstanceService customEntityInstanceService;
+	private CrossStorageService crossStorageService;
 
-	public void findBinary(Boolean showOnExplorer, String repositoryCode, String cetCode, String uuid, String cftCode, HttpServletResponse response)
-			throws EntityDoesNotExistsException, BusinessApiException, IOException {
+	public void findBinary(Boolean showOnExplorer, String repositoryCode, String cetCode, String uuid, String cftCode, HttpServletResponse response) throws EntityDoesNotExistsException, BusinessApiException, IOException {
 
-		CustomFieldTemplate cft = cftService.findByCode(cftCode);
+		// Retrieve CET
+		CustomEntityTemplate cet = cache.getCustomEntityTemplate(cetCode);
+		if (cet == null) {
+			throw new EntityDoesNotExistsException(CustomEntityTemplate.class, cetCode);
+		}
 
+		// Retrieve CFT
+		CustomFieldTemplate cft = cache.getCustomFieldTemplate(cftCode, cet.getAppliesTo());
 		if (cft == null) {
 			throw new EntityDoesNotExistsException(CustomFieldTemplate.class, cftCode);
 		}
 
-		// get the entity
-		CustomEntityInstance cei = customEntityInstanceService.findByUuid(cetCode, uuid);
+		// Retrieve repository
+		Repository repository = repositoryService.findByCode(repositoryCode);
+		if(repository == null){
+			throw new EntityDoesNotExistsException(Repository.class, repositoryCode);
+		}
 
-		String cfValue = (String) cfiService.getCFValue(cei, cftCode);
-		String filename = FilenameUtils.getName(cfValue);
+		// Retrieve the entity
+		Map<String, Object> values = crossStorageService.find(repository, cet, uuid);
+		if(values == null){
+			throw new EntityDoesNotExistsException("EntityInstance", uuid);
+		}
+
+		String filename = null;
+
+		Object filePathOrfilePaths = values.get(cftCode);
+		if(filePathOrfilePaths instanceof String){
+			filename = (String) filePathOrfilePaths;
+		} else if(filePathOrfilePaths instanceof Collection){
+			filename = (String) ((Collection) filePathOrfilePaths).iterator().next();
+		} else if(filePathOrfilePaths == null){
+			// No file stored
+			response.sendError(404, "No binary stored for entity " + uuid + "[" + cetCode + "." + cftCode + "]");
+			return;
+		}
 
 		BinaryStoragePathParam params = new BinaryStoragePathParam();
 		params.setRootPath(repositoryCode);
