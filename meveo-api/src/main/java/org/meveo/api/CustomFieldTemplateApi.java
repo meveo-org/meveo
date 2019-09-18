68232697909c59f3f73eae5d21eed33f240cdf0c
Clément Bareth <clement.bareth@web-drone.fr>
2019-09-18 14:59:47 +0200

    Fix of NPE when checking if CFT is stored is Neo4J
    Signed-off-by: Clément Bareth <clement.bareth@web-drone.fr>

--- meveo-api/src/main/java/org/meveo/api/CustomFieldTemplateApi.java
+++ meveo-api/src/main/java/org/meveo/api/CustomFieldTemplateApi.java
@@ -141,7 +141,7 @@ public class CustomFieldTemplateApi extends BaseApi {
             missingParameters.add("appliesTo");
         }
         
-        if(postData.getFieldType() == CustomFieldTypeEnum.ENTITY && postData.getStorages().contains(DBStorageType.NEO4J) && postData.getRelationshipName() == null){
+        if(postData.getFieldType() == CustomFieldTypeEnum.ENTITY && postData.getStorages() != null && postData.getStorages().contains(DBStorageType.NEO4J) && postData.getRelationshipName() == null){
        	 	missingParameters.add("relationshipName");
         }
 
