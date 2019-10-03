7e3bf73d7c96bfc9f647c80d67416137d8e1f4f4
hien.bach <!Nhi08052008!!>
2019-10-03 14:38:09 +0700

    re #3897 -Revert commit wrong branch

--- meveo-admin/web/src/main/java/org/meveo/admin/action/storage/GitRepositoryBean.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/storage/GitRepositoryBean.java
@@ -3,6 +3,7 @@ package org.meveo.admin.action.storage;
 import org.jboss.seam.international.status.builder.BundleKey;
 import org.meveo.admin.action.BaseCrudBean;
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.admin.web.interceptor.ActionMethod;
 import org.meveo.api.BaseCrudApi;
 import org.meveo.api.dto.git.GitRepositoryDto;
 import org.meveo.api.git.GitRepositoryApi;
@@ -13,6 +14,7 @@ import org.meveo.service.git.GitClient;
 import org.meveo.service.git.GitRepositoryService;
 import org.slf4j.Logger;
 
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -43,9 +45,13 @@ public class GitRepositoryBean extends BaseCrudBean<GitRepository, GitRepository
         super(GitRepository.class);
     }
 
-    @Override
-    public String saveOrUpdate(boolean killConversation) throws BusinessException, ELException {
-        return super.saveOrUpdate(killConversation);
+    @ActionMethod
+    public String saveOrUpdateGit() throws BusinessException, ELException {
+        String result = saveOrUpdate(false);
+        if (result == null) {
+            FacesContext.getCurrentInstance().validationFailed();
+        }
+        return result;
     }
 
     public void pushRemote() {
