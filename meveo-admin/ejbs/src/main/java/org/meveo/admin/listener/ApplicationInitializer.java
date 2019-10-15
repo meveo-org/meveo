b0641ff81af52013685e39dbfecf5dd71cefcd3d
Clément Bareth <clement.bareth@web-drone.fr>
2019-10-15 10:06:37 +0200

    Always popuplate all ontology caches
    Signed-off-by: Clément Bareth <clement.bareth@web-drone.fr>

--- meveo-admin/ejbs/src/main/java/org/meveo/admin/listener/ApplicationInitializer.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/listener/ApplicationInitializer.java
@@ -142,7 +142,7 @@ public class ApplicationInitializer {
 
         // Initialize caches
         notifCache.populateCache(System.getProperty(CacheContainerProvider.SYSTEM_PROPERTY_CACHES_TO_LOAD));
-        cftCache.populateCache(System.getProperty(CacheContainerProvider.SYSTEM_PROPERTY_CACHES_TO_LOAD), true);
+        cftCache.populateCache(null, true);
         jobCache.populateCache(System.getProperty(CacheContainerProvider.SYSTEM_PROPERTY_CACHES_TO_LOAD));
 
 
