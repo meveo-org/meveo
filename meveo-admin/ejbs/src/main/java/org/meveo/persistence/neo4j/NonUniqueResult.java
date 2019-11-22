44df048d7638120a52cde9b46e3399b40a49178f
Clément Bareth <clement.bareth@web-drone.fr>
2019-11-22 16:00:40 +0100

    Fix cross storage issues caused by NullPointers
    Signed-off-by: Clément Bareth <clement.bareth@web-drone.fr>

---
+++ meveo-admin/ejbs/src/main/java/org/meveo/persistence/neo4j/NonUniqueResult.java
@@ -0,0 +1,38 @@
+/*
+ * (C) Copyright 2018-2020 Webdrone SAS (https://www.webdrone.fr/) and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. This program is
+ * not suitable for any direct or indirect application in MILITARY industry See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.meveo.persistence.neo4j;
+
+import org.neo4j.driver.v1.Record;
+
+import java.util.List;
+
+public class NonUniqueResult extends RuntimeException {
+
+    List<Record> records;
+
+    public NonUniqueResult(List<Record> records) {
+        this.records = records;
+    }
+
+    public List<Record> getRecords() {
+        return records;
+    }
+
+    public void setRecords(List<Record> records) {
+        this.records = records;
+    }
+}
