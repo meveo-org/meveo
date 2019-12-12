00e54677a7f4960e09535da1f322f0689c06b745
Clément Bareth <clement.bareth@web-drone.fr>
2019-12-12 14:49:40 +0100

    Refs #3980 : Bad reference entity exported on Custom Table CSV export
    Problem solved by cleaning the code

--- meveo-admin/ejbs/src/main/java/org/meveo/service/custom/CustomTableService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/custom/CustomTableService.java
@@ -408,26 +408,20 @@ public class CustomTableService extends NativePersistenceService {
                     List<Map<String, Object>> values = query.list();
 
                     /* Fetch entity references */
-                    Map<Integer, Object[]> fetchedEntityReferences = new HashMap<>();			// Map used to replace data with as key the index of the value to replace and as value the actual (key, value) pair to replace
                     Map<String, Map<String, String>> entityReferencesCache = new HashMap<>();	// Cache used to avoid fetching multiple time the same data
-                    values.forEach(map -> map.forEach((key, value) -> fields.stream()
-                            .filter(f -> f.getDbFieldname().equals(key)).findFirst()
-                            .ifPresent(customFieldTemplate -> {
-                                if(customFieldTemplate.getFieldType() == CustomFieldTypeEnum.ENTITY) {
-                                    String referencedEntity = entityReferencesCache.computeIfAbsent(key, k -> new HashMap<>())
-                                        .computeIfAbsent(
-                                            customFieldTemplate.getDbFieldname(),
-                                            k -> {
-                                                log.info("Fetching {} with uuid {}", customFieldTemplate.getCode(), value);
-                                                Map<String, Object> entityRefValues = findById(k, (String) value);
-                                                entityRefValues.remove("uuid");				// We don't want to save the uuid
-                                                return JacksonUtil.toString(entityRefValues);
-                                            }
-                                    );
-                                    fetchedEntityReferences.put(values.indexOf(map), new Object[]{key, referencedEntity});
-                                }
-                            })));
-                    fetchedEntityReferences.forEach((index, array) -> values.get(index).put((String) array[0], array[1])); 
+                    for(Map<String, Object> map : values) {
+                    	for(Entry<String, Object> entry : new HashMap<>(map).entrySet()) {
+                    		for(CustomFieldTemplate field : fields) {
+                    			if(field.getDbFieldname().equals(entry.getKey()) && field.getFieldType() == CustomFieldTypeEnum.ENTITY) {
+                    				String referencedEntity = entityReferencesCache
+                    						.computeIfAbsent((String) entry.getValue(), k -> new HashMap<>())
+                    						.computeIfAbsent(field.getDbFieldname(), fieldName -> fetchField(entry.getValue(), field, fieldName));
+                    				
+                    				map.put(entry.getKey(), referencedEntity);
+                    			}
+                    		}
+                    	}
+                    }
                     
                     nrItemsFound = values.size();
                     firstRow = firstRow + 500;
@@ -447,6 +441,19 @@ public class CustomTableService extends NativePersistenceService {
             return new AsyncResult<>(new DataImportExportStatistics(e));
         }
     }
+
+	/**
+	 * @param id
+	 * @param field
+	 * @param tableName
+	 * @return
+	 */
+	public String fetchField(Object id, CustomFieldTemplate field, String tableName) {
+		log.info("Fetching {} with uuid {}", field.getCode(), id);
+		Map<String, Object> entityRefValues = findById(tableName, (String) id);
+		entityRefValues.remove("uuid");				// We don't want to save the uuid
+		return JacksonUtil.toString(entityRefValues);
+	}
 
     /**
      * Import data into custom table
