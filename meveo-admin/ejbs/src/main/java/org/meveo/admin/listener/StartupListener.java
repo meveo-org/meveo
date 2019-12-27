32cb59ef7f15396d0383e8d823fe4f693f48eb03
hien.bach <hien.bach@manaty.net>
2019-12-27 19:55:41 +0700

    re #4005 - Default Repository
    - None of the default SQL Configuration nor the default Repository can be deleted nor update.

--- meveo-admin/ejbs/src/main/java/org/meveo/admin/listener/StartupListener.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/admin/listener/StartupListener.java
@@ -25,8 +25,15 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 
 import org.hibernate.Session;
+import org.meveo.admin.exception.BusinessException;
+import org.meveo.commons.utils.ParamBean;
 import org.meveo.jpa.EntityManagerWrapper;
 import org.meveo.jpa.MeveoJpa;
+import org.meveo.model.sql.SqlConfiguration;
+import org.meveo.model.storage.Repository;
+import org.meveo.persistence.sql.SQLConnectionProvider;
+import org.meveo.persistence.sql.SqlConfigurationService;
+import org.meveo.service.storage.RepositoryService;
 import org.slf4j.Logger;
 
 /**
@@ -39,6 +46,12 @@ public class StartupListener {
 
 	@Inject
 	private ApplicationInitializer applicationInitializer;
+
+	@Inject
+	private RepositoryService repositoryService;
+
+	@Inject
+	private SqlConfigurationService sqlConfigurationService;
 
 	@Inject
 	private Logger log;
@@ -56,5 +69,44 @@ public class StartupListener {
 			applicationInitializer.init();
 			log.info("Thank you for running Meveo Community code.");
 		});
+
+		// A default Repository and SQL Configuration should be genarated/updated at Meveo first initialization
+		try {
+			SqlConfiguration defaultSqlConfiguration;
+			Repository defaultRepository;
+			defaultSqlConfiguration = sqlConfigurationService.findByCode("default");
+			if (defaultSqlConfiguration == null) {
+				defaultSqlConfiguration = new SqlConfiguration();
+				defaultSqlConfiguration.setCode("default");
+				setSqlConfiguration(defaultSqlConfiguration);
+				sqlConfigurationService.create(defaultSqlConfiguration);
+			} else {
+				setSqlConfiguration(defaultSqlConfiguration);
+				sqlConfigurationService.update(defaultSqlConfiguration);
+			}
+			defaultRepository = repositoryService.findByCode("default");
+			if (defaultRepository == null) {
+				defaultRepository = new Repository();
+				defaultRepository.setCode("default");
+				defaultRepository.setSqlConfiguration(defaultSqlConfiguration);
+				repositoryService.create(defaultRepository);
+			}
+		} catch (BusinessException e) {}
 	}
+
+	private SqlConfiguration setSqlConfiguration(SqlConfiguration sqlConfiguration) {
+		String driverClass = ParamBean.getInstance().getProperty("sql.driverClass", "org.postgresql.Driver");
+		String url = ParamBean.getInstance().getProperty("sql.url", "jdbc:postgresql://localhost/meveo");
+		String username = ParamBean.getInstance().getProperty("sql.username", "meveo");
+		String password = ParamBean.getInstance().getProperty("sql.password", "meveo");
+		String dialect = ParamBean.getInstance().getProperty("sql.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+
+		sqlConfiguration.setDriverClass(driverClass);
+		sqlConfiguration.setUrl(url);
+		sqlConfiguration.setUsername(username);
+		sqlConfiguration.setPassword(password);
+		sqlConfiguration.setDialect(dialect);
+		return sqlConfiguration;
+	}
+
 }
