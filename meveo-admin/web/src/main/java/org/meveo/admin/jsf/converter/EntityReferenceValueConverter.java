5a0b5d2cb57b1b8f9c293ace49e18ca75cfb9054
Clément Bareth <clement.bareth@web-drone.fr>
2020-07-07 12:31:42 +0200

    Fix # 40 : can't save new scripting job

--- meveo-admin/web/src/main/java/org/meveo/admin/jsf/converter/EntityReferenceValueConverter.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/jsf/converter/EntityReferenceValueConverter.java
@@ -1,55 +1,71 @@
 package org.meveo.admin.jsf.converter;
 
-import org.meveo.commons.utils.StringUtils;
-import org.meveo.model.BusinessEntity;
-import org.meveo.service.crm.impl.CustomFieldInstanceService;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.spi.CDI;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
-import java.io.Serializable;
+
+import org.meveo.cache.CacheKeyStr;
+import org.meveo.commons.utils.StringUtils;
+import org.meveo.model.BusinessEntity;
+import org.meveo.service.crm.impl.CustomFieldInstanceService;
 
 /**
  * @author Phu Bach | pbach1982@gmail.com
- * @version 6.9.0
+ * @author clement.bareth
+ * @version 6.10.0
  **/
 @FacesConverter("entityReferenceValueConverter")
-@ApplicationScoped
+@ViewScoped
 public class EntityReferenceValueConverter implements Converter<Object>, Serializable {
 
     private static final long serialVersionUID = 2297474050618191644L;
 
-    @Inject
-    private CustomFieldInstanceService customFieldInstanceService;
-
+//    @Inject
+//    private CustomFieldInstanceService customFieldInstanceService;
+    
+    private Map<CacheKeyStr, Object> entityReferencesCache = new HashMap<>();
+    
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
         String classname = (String) component.getAttributes().get("classname");
-        if(customFieldInstanceService == null) {
-            customFieldInstanceService = CDI.current().select(CustomFieldInstanceService.class).get();
-        }
+        
+//        if(customFieldInstanceService == null) {
+//            customFieldInstanceService = CDI.current().select(CustomFieldInstanceService.class).get();
+//        }
+        
         if (!StringUtils.isBlank(classname)) {
-            BusinessEntity convertedEntity = customFieldInstanceService.findBusinessEntityCFVByCode(classname, value);
-            if (convertedEntity != null) {
-                return convertedEntity;
-            } else {
-                return null;
-            }
-        } else {
-            return null;
+            //BusinessEntity convertedEntity = customFieldInstanceService.findBusinessEntityCFVByCode(classname, value);
+            //if (convertedEntity != null) {
+             //   return convertedEntity;
+            //} else {
+                CacheKeyStr key = new CacheKeyStr(classname, value);
+                return entityReferencesCache.get(key);
+            //}
         }
+        
+        return null;
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
         String str = "";
+        
         if (value instanceof BusinessEntity) {
             str = "" + ((BusinessEntity) value).getCode();
         }
+        
+        CacheKeyStr key = new CacheKeyStr(value.getClass().getName(), str);
+        entityReferencesCache.put(key, value);
+        
         return str;
     }
+    
 }
