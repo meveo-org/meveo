57db8092696d0dc58403e88aa17d804e3e651678
arthur.grenier <arthur.grenier@DESKTOP-I5F09RF.webdrone.fr>
2021-01-18 11:23:35 +0100

    Add services for moving an entity from a module to an other #258

--- meveo-admin/ejbs/src/main/java/org/meveo/service/base/BusinessService.java
+++ meveo-admin/ejbs/src/main/java/org/meveo/service/base/BusinessService.java
@@ -22,14 +22,24 @@ package org.meveo.service.base;
 import org.hibernate.LockOptions;
 import org.hibernate.NaturalIdLoadAccess;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.meveo.api.dto.BusinessEntityDto;
+import org.meveo.api.dto.module.MeveoModuleDto;
 import org.meveo.commons.utils.QueryBuilder;
 import org.meveo.commons.utils.QueryBuilder.QueryLikeStyleEnum;
 import org.meveo.commons.utils.StringUtils;
 import org.meveo.model.BusinessEntity;
-
+import org.meveo.model.ModuleItem;
+import org.meveo.model.module.MeveoModule;
+import org.meveo.model.persistence.JacksonUtil;
 import javax.persistence.NoResultException;
 import javax.persistence.NonUniqueResultException;
-import javax.persistence.TypedQuery;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -39,7 +49,7 @@ import java.util.List;
  * @param <P> extension of Business Entity.
  */
 public abstract class BusinessService<P extends BusinessEntity> extends PersistenceService<P> {
-
+	
     /**
      * Find entity by code - strict match.
      * 
@@ -167,6 +177,87 @@ public abstract class BusinessService<P extends BusinessEntity> extends Persiste
             log.error("More than one entity of type {} with code {} found", entityClass, code);
             return null;
         }
+    }
+
+    /** 
+     * Find which the module the entity belongs to 
+
+     * @param entity
+     * @return MeveoModule, or null otherwise
+     */
+    @SuppressWarnings({ "rawtypes", "deprecation" })
+	public MeveoModule findModuleOf(P entity){
+    	MeveoModule module = null;
+    	if (entity != null) {
+    		Session session = this.getEntityManager().unwrap(Session.class);
+    		Query q = session.createQuery("SELECT m.module FROM ModuleItem m WHERE m.code = :code AND m.ItemType = :itemType");
+    		q.setString("code", entity.getCode());
+    		q.setString("itemType", entity.getClass().getName());
+    		module = (MeveoModule) q.getResultList().get(0);
+    	}
+    	return module;
+    }
+    
+    /**
+     * Remove the entity that belongs to the module
+     * 
+     * @param entity
+     * @param module
+     */
+    public void removeFilesFromModule(P entity, MeveoModule module) {
+    	MeveoModuleDto meveoModuleDto = new MeveoModuleDto(module);
+    	BusinessEntityDto businessEntityDto = new BusinessEntityDto(entity);
+    	File fileToDelete = new File("..\\git\\"+meveoModuleDto.getCode()+"\\"+businessEntityDto.getClass().getAnnotation(ModuleItem.class).path()+"\\"+entity.getCode()+".json");
+
+    	fileToDelete.delete();
+    }
+    
+    /**
+     * Create the entity in the dedicated module
+     * 
+     * @param entity
+     * @param module
+     */
+    public void addFilesToModule(P entity, MeveoModule module) {
+    	MeveoModuleDto meveoModuleDto = new MeveoModuleDto(module);
+    	BusinessEntityDto businessEntityDto = new BusinessEntityDto(entity);
+    	String businessEntityDtoSerialize = JacksonUtil.toString(businessEntityDto);
+    	
+    	try {
+	    	File file = new File("..\\git\\"+meveoModuleDto.getCode()+"\\"+businessEntityDto.getClass().getAnnotation(ModuleItem.class).path()+"\\"+entity.getCode()+".json");
+	    	file.createNewFile();
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	try {
+    		Path path = Paths.get("..\\git\\"+meveoModuleDto.getCode()+"\\"+businessEntityDto.getClass().getAnnotation(ModuleItem.class).path()+"\\"+entity.getCode()+".json");
+    		byte[] strToBytes = businessEntityDtoSerialize.getBytes();
+
+    		Files.write(path, strToBytes);
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    /**
+     * Retrieve the current module of the entity, ignore if none
+     * Remove the files from old Module
+     * Add the files to the new module
+     * 
+     * @param entity
+     * @param module
+     */
+    public void moveFilesToModule(P entity, MeveoModule module) {
+    	MeveoModule currentModule = findModuleOf(entity);
+    	if (currentModule != null) {
+    		removeFilesFromModule(entity, currentModule);
+    		
+	    	if ((module != null)) {
+	    		addFilesToModule(entity, module);
+	    	}
+    	}
+    	
     }
 
     // ------------------------------- Methods that retrieves lazy loaded objects ----------------------------------- //
