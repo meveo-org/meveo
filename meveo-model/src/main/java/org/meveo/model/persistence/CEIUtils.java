37ae07141e7bc1385ded38745308ee785990ad4c
Clément Bareth <clement.bareth@gmail.com>
2021-03-16 11:16:42 +0100

    CEIUtils : function to convert list of custom entity instances to list of EntityOrRelation #323

--- meveo-model/src/main/java/org/meveo/model/persistence/CEIUtils.java
+++ meveo-model/src/main/java/org/meveo/model/persistence/CEIUtils.java
@@ -9,26 +9,31 @@ import java.lang.reflect.Method;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import org.meveo.commons.utils.ReflectionUtils;
+import org.meveo.interfaces.EntityGraph;
+import org.meveo.interfaces.EntityRelation;
 import org.meveo.model.CustomEntity;
 import org.meveo.model.crm.CustomFieldTemplate;
 import org.meveo.model.crm.custom.CustomFieldTypeEnum;
 import org.meveo.model.crm.custom.CustomFieldValues;
 import org.meveo.model.customEntities.CustomEntityInstance;
 import org.meveo.model.customEntities.CustomEntityTemplate;
+import org.meveo.model.customEntities.annotations.Relation;
+import org.meveo.model.typereferences.GenericTypeReferences;
 import org.meveo.security.PasswordUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 
 /**
@@ -39,9 +44,9 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
  * @version 6.12.0
  */
 public class CEIUtils {
-	
+
 	private static Logger LOG = LoggerFactory.getLogger(CEIUtils.class);
-	
+
 	/**
 	 * @param cei  The entity to hash
 	 * @param cfts Custom fields of the entity template
@@ -49,26 +54,111 @@ public class CEIUtils {
 	 */
 	public static String getHash(CustomEntityInstance cei, Map<String, CustomFieldTemplate> cfts) {
 		List<Object> objectsToHash = new ArrayList<>();
-		if(cei.getUuid() == null) {
+		if (cei.getUuid() == null) {
 			throw new IllegalArgumentException("Can't hash a CEI without uuid");
 		}
 		objectsToHash.add(cei.getUuid());
-		
+
 		cei.getCfValuesAsValues().forEach((key, value) -> {
-			if(value != null && cfts.get(key) != null && cfts.get(key).getFieldType() != CustomFieldTypeEnum.SECRET) {
+			if (value != null && cfts.get(key) != null && cfts.get(key).getFieldType() != CustomFieldTypeEnum.SECRET) {
 				objectsToHash.add(value);
 			}
 		});
-		
+
 		return PasswordUtils.getSalt(objectsToHash.toArray());
 	}
-	
+
+	@SuppressWarnings("unchecked")
+	public static EntityGraph toEntityGraph(Collection<CustomEntity> customEntities) {
+		List<org.meveo.interfaces.Entity> entities = new ArrayList<>();
+		List<EntityRelation> relations = new ArrayList<>();
+		EntityGraph entityGraph = new EntityGraph(entities, relations);
+		
+		for(var customEntity : customEntities) {
+			if(customEntity.getUuid() == null) {
+				setUUIDField(customEntity, UUID.randomUUID().toString());
+			}
+			
+			Class<? extends CustomEntity> customClass = customEntity.getClass();
+
+			var fields = JacksonUtil.convert(customEntity, GenericTypeReferences.MAP_STRING_OBJECT);
+			var relationshipsFields = new HashMap<String, Object>();
+			
+			// Retain all fields annotated with @Relation
+			for(var field : Map.copyOf(fields).entrySet()) {
+				var fieldDef = ReflectionUtils.getField(customClass, field.getKey());
+				if(fieldDef.isAnnotationPresent(Relation.class)) {
+					relationshipsFields.put(field.getKey(), field.getValue());
+					fields.remove(field.getKey());
+				}
+			}
+			
+			var entity = new org.meveo.interfaces.Entity.Builder()
+		        .name(customEntity.getUuid())
+		        .type(customEntity.getCetCode())
+		        .properties(fields)
+		        .build();
+			
+			entityGraph.getEntities().add(entity);
+			
+			relationshipsFields.keySet().forEach(field -> {
+				var fieldDef = ReflectionUtils.getField(customClass, field);
+				var value = ReflectionUtils.findValueWithGetter(customEntity, field);
+				EntityGraph subGraph = null;
+				if(value.isPresent() && value.get() instanceof Collection) {
+					final var customEntitiesValue = (Collection<CustomEntity>) value.get();
+					subGraph = toEntityGraph(List.copyOf(customEntitiesValue));
+					for(var relatedCustomEntity : customEntitiesValue){
+						subGraph.getEntities()
+								.stream()
+								.filter(e -> e.getName().equals(relatedCustomEntity.getUuid()))
+								.findFirst()
+								.ifPresent(e -> {
+									var relation = new org.meveo.interfaces.EntityRelation.Builder()
+											.name(UUID.randomUUID().toString())
+											.source(entity)
+											.target(e)
+									        .type(fieldDef.getAnnotation(Relation.class).value())
+									        .properties(Map.of())
+									        .build();
+									entityGraph.getRelations().add(relation);
+								});
+					}
+				} else if(value.isPresent() && value.get() instanceof CustomEntity){
+					CustomEntity customEntityValue = (CustomEntity) value.get();
+					subGraph = toEntityGraph(List.of(customEntityValue));
+					subGraph.getEntities()
+					.stream()
+					.filter(e -> e.getName().equals(customEntityValue.getUuid()))
+					.findFirst()
+					.ifPresent(e -> {
+						var relation = new org.meveo.interfaces.EntityRelation.Builder()
+								.name(UUID.randomUUID().toString())
+								.source(entity)
+								.target(e)
+						        .type(fieldDef.getAnnotation(Relation.class).value())
+						        .properties(Map.of())
+						        .build();
+						entityGraph.getRelations().add(relation);
+					});
+				}
+				
+				if(subGraph != null) {
+					entityGraph.getEntities().addAll(subGraph.getEntities());
+					entityGraph.getRelations().addAll(subGraph.getRelations());
+				}
+			});
+		}
+		
+		return entityGraph;
+	}
+
 	public static CustomEntityInstance fromMap(Map<String, Object> map, CustomEntityTemplate cet) {
 		var cei = pojoToCei(map);
 		cei.setCet(cet);
 		return cei;
 	}
-	
+
 	/**
 	 * Converts a POJO to a CEI
 	 * 
@@ -77,40 +167,34 @@ public class CEIUtils {
 	 */
 	public static CustomEntityInstance pojoToCei(Object pojo) {
 		Map<String, Object> pojoAsMap;
-		if(pojo instanceof Map) {
+		if (pojo instanceof Map) {
 			pojoAsMap = (Map<String, Object>) pojo;
-		} else { 
+		} else {
 			// Transform POJO into Map
 			Map<String, Object> values = new HashMap<>();
-			Stream.of(pojo.getClass().getMethods())
-				.filter(m -> m.getName().startsWith("get") | m.getName().startsWith("is"))
-				.filter(m -> m.getParameterCount() == 0)
-				.forEach(m -> {
-					var key = getFieldForGetter(pojo.getClass(), m);
-					try {
-						if(key != null) {
-							var value = m.invoke(pojo);
-							values.put(key, value);
-						}
-					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-						throw new RuntimeException(e);
+			Stream.of(pojo.getClass().getMethods()).filter(m -> m.getName().startsWith("get") | m.getName().startsWith("is")).filter(m -> m.getParameterCount() == 0).forEach(m -> {
+				var key = getFieldForGetter(pojo.getClass(), m);
+				try {
+					if (key != null) {
+						var value = m.invoke(pojo);
+						values.put(key, value);
 					}
-				});
-				
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					throw new RuntimeException(e);
+				}
+			});
+
 			pojoAsMap = new HashMap<>();
-			values.entrySet()
-				.stream()
-				.forEach(e -> {
-					if(e.getValue() != null && e.getValue().getClass().getAnnotation(Entity.class) != null) {
-						pojoAsMap.put(e.getKey(), getIdValue(e.getValue()));
-					} else {
-						pojoAsMap.put(e.getKey(), e.getValue());
-					}
-				});
+			values.entrySet().stream().forEach(e -> {
+				if (e.getValue() != null && e.getValue().getClass().getAnnotation(Entity.class) != null) {
+					pojoAsMap.put(e.getKey(), getIdValue(e.getValue()));
+				} else {
+					pojoAsMap.put(e.getKey(), e.getValue());
+				}
+			});
 		}
-				
-				
-				//JacksonUtil.convert(pojo, GenericTypeReferences.MAP_STRING_OBJECT);
+
+		// JacksonUtil.convert(pojo, GenericTypeReferences.MAP_STRING_OBJECT);
 		CustomEntityInstance cei = new CustomEntityInstance();
 		cei.setUuid((String) pojoAsMap.get("uuid"));
 		cei.setCetCode(pojo.getClass().getSimpleName());
@@ -119,12 +203,12 @@ public class CEIUtils {
 		cei.setCfValues(customFieldValues);
 		return cei;
 	}
-	
+
 	/**
 	 * Converts a CEI to a POJO
 	 * 
-	 * @param <T> type of the pojo
-	 * @param cei the cei to convert
+	 * @param <T>       type of the pojo
+	 * @param cei       the cei to convert
 	 * @param pojoClass the class of the pojo
 	 * @return the instance of the POJO
 	 */
@@ -133,38 +217,29 @@ public class CEIUtils {
 		pojoValues.put("uuid", cei.getUuid());
 		return deserialize(pojoValues, pojoClass);
 	}
-	
+
 	private static String getFieldForGetter(Class<?> clazz, Method getter) {
 		String fieldName;
-		if(getter.getName().startsWith("is")) {
+		if (getter.getName().startsWith("is")) {
 			fieldName = getter.getName().substring(2);
 		} else {
 			fieldName = getter.getName().substring(3);
 		}
-		
-		return ReflectionUtils.getAllFields(new ArrayList<>(),clazz)
-			.stream()
-			.filter(f -> f.getName().toUpperCase().equals(fieldName.toUpperCase()))
-			.findFirst()
-			.map(Field::getName)
-			.orElse(null);
+
+		return ReflectionUtils.getAllFields(new ArrayList<>(), clazz).stream().filter(f -> f.getName().toUpperCase().equals(fieldName.toUpperCase())).findFirst().map(Field::getName).orElse(null);
 	}
-	
+
 	private static void setIdField(Object object, Object id) {
-		ReflectionUtils.getAllFields(new ArrayList<>(),object.getClass())
-			.stream()
-			.filter(f -> f.getAnnotation(Id.class) != null)
-			.findFirst()
-			.ifPresent(f -> {
-				try {
-					f.setAccessible(true);
-					f.set(object, id);
-				} catch (IllegalArgumentException | IllegalAccessException e) {
-					throw new RuntimeException(e);
-				}
-			});
+		ReflectionUtils.getAllFields(new ArrayList<>(), object.getClass()).stream().filter(f -> f.getAnnotation(Id.class) != null).findFirst().ifPresent(f -> {
+			try {
+				f.setAccessible(true);
+				f.set(object, id);
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		});
 	}
-	
+
 	private static void setUUIDField(Object object, String value) {
 		var setter = findSetter("uuid", object.getClass());
 		if (setter != null) {
@@ -175,25 +250,21 @@ public class CEIUtils {
 			}
 		}
 	}
-	
+
 	private static Object getIdValue(Object object) {
-		return ReflectionUtils.getAllFields(new ArrayList<>(),object.getClass())
-				.stream()
-				.filter(f -> f.getAnnotation(Id.class) != null)
-				.findFirst()
-				.map(f -> {
-					try {
-						f.setAccessible(true);
-						return f.get(object);
-					} catch (IllegalArgumentException | IllegalAccessException e) {
-						throw new RuntimeException(e);
-					}
-				}).orElse(null);
+		return ReflectionUtils.getAllFields(new ArrayList<>(), object.getClass()).stream().filter(f -> f.getAnnotation(Id.class) != null).findFirst().map(f -> {
+			try {
+				f.setAccessible(true);
+				return f.get(object);
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		}).orElse(null);
 	}
-	
+
 	/**
 	 * @param value
-	 * @return 
+	 * @return
 	 * @throws RuntimeException
 	 */
 	public static <T> T deserialize(Map<String, Object> value, Class<T> clazz) throws RuntimeException {
@@ -208,9 +279,7 @@ public class CEIUtils {
 
 				// if type extends CustomEntity set the UUID
 				if (CustomEntity.class.isAssignableFrom(paramType)) {
-					lazyInitInstance = paramType
-							.getDeclaredConstructor()
-							.newInstance();
+					lazyInitInstance = paramType.getDeclaredConstructor().newInstance();
 					setUUIDField(lazyInitInstance, (String) entry.getValue());
 					setter.invoke(instance, lazyInitInstance);
 
@@ -223,9 +292,7 @@ public class CEIUtils {
 
 					} catch (IllegalArgumentException e) {
 						try {
-							lazyInitInstance = paramType
-									.getDeclaredConstructor()
-									.newInstance();
+							lazyInitInstance = paramType.getDeclaredConstructor().newInstance();
 							setIdField(lazyInitInstance, entry.getValue());
 							setter.invoke(instance, lazyInitInstance);
 
@@ -235,7 +302,7 @@ public class CEIUtils {
 								Instant val = ((Timestamp) entry.getValue()).toInstant();
 								setter.invoke(instance, val);
 							} else {
-								LOG.error("Failed to deserialize {}", entry.getValue(),e);
+								LOG.error("Failed to deserialize {}", entry.getValue(), e);
 							}
 						}
 					}
@@ -249,10 +316,7 @@ public class CEIUtils {
 	}
 
 	private static Method findSetter(String fieldName, Class<?> clazz) {
-		return Stream.of(clazz.getMethods())
-			.filter(m -> m.getName().toUpperCase().equals("SET" + fieldName.toUpperCase()))
-			.findFirst()
-			.orElseThrow(() -> new IllegalArgumentException("No setter found for field " + fieldName + " in " + clazz));
+		return Stream.of(clazz.getMethods()).filter(m -> m.getName().toUpperCase().equals("SET" + fieldName.toUpperCase())).findFirst().orElseThrow(() -> new IllegalArgumentException("No setter found for field " + fieldName + " in " + clazz));
 	}
 
 }
