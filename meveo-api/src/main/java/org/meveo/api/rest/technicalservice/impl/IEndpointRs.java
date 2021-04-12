5be3fd043b23d79cf3143e16b34fbea08bf026fa
Clément Bareth <clement.bareth@gmail.com>
2021-04-12 16:10:47 +0200

    Move annotation to interface

--- meveo-api/src/main/java/org/meveo/api/rest/technicalservice/impl/IEndpointRs.java
+++ meveo-api/src/main/java/org/meveo/api/rest/technicalservice/impl/IEndpointRs.java
@@ -5,6 +5,8 @@ package org.meveo.api.rest.technicalservice.impl;
 
 import java.io.IOException;
 
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -27,57 +29,79 @@ import org.meveo.api.dto.technicalservice.endpoint.EndpointDto;
 import org.meveo.api.exception.EntityDoesNotExistsException;
 import org.meveo.model.technicalservice.endpoint.Endpoint;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
-/**
- * 
- * @author clement.bareth
- * @since 
- * @version
- */
+@Path("/endpoint")
+@DeclareRoles({ "endpointManagement" })
+@RolesAllowed({ "endpointManagement" })
+@Api("EndpointRs")
 public interface IEndpointRs {
 
-	Response create(EndpointDto endpointDto) throws BusinessException;
+	@POST
+	Response create(@Valid @NotNull EndpointDto endpointDto) throws BusinessException;
+	
+	@PUT
+	Response createOrReplace(@Valid @NotNull EndpointDto endpointDto) throws BusinessException;
 
-	Response createOrReplace(EndpointDto endpointDto) throws BusinessException;
-
-	Response list(String serviceCode);
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	Response list(@QueryParam("service") String serviceCode);
 
 	/**
 	 * Delete a {@link Endpoint}
 	 *
 	 * @param code Code of the {@link Endpoint} to delete
 	 */
-	Response delete(String code) throws BusinessException, EntityDoesNotExistsException;
+	@DELETE
+	@Path("/{code}")
+	@ApiOperation(value = "Delete endpoint")
+	Response delete(@PathParam("code") @NotNull @ApiParam("Code of the endpoint") String code) throws BusinessException, EntityDoesNotExistsException;
 
 	/**
 	 * Find a {@link Endpoint} by code
 	 *
 	 * @param code Code of the {@link Endpoint} to find
 	 */
-	Response find(String code);
+	
+	@GET
+	@Path("/{code}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Find endpoint by code")
+	Response find(@PathParam("code") @NotNull @ApiParam("Code of the endpoint")  String code);
 
 	/**
 	 * Check exist a {@link Endpoint}
 	 *
 	 * @param code Code of the {@link Endpoint} to check
 	 */
-	Response exists(String code);
+	@HEAD
+	@Path("/{code}")
+	@ApiOperation(value = "Check exist an endpoint")
+	Response exists(@PathParam("code") @NotNull @ApiParam("Code of the endpoint") String code);
 
 	/**
 	 * Get script of a {@link Endpoint}
 	 *
 	 * @param code Code of the {@link Endpoint} to get script
 	 */
-	String getScript(String code, HttpServletRequest servletRequest) throws EntityDoesNotExistsException, IOException;
+	@GET
+	@Path("/{code}.js")
+    @Cache(maxAge = 86400)
+	@Produces("application/javascript")
+	@ApiOperation(value = " Get script of the endpoint")
+	String getScript(@PathParam("code") @ApiParam("Code of the endpoint") String code, @Context HttpServletRequest servletRequest) throws EntityDoesNotExistsException, IOException;
 
 	/**
 	 * Generate open api json of a {@link Endpoint}
 	 *
 	 * @param code Code of the {@link Endpoint} to generate open api json
 	 */
-	Response generateOpenApiJson(String code);
+	@GET
+	@Path("/openApi/{code}")
+	@ApiOperation(value = "Generate open api json of the endpoint")
+	Response generateOpenApiJson(@PathParam("code") @NotNull @ApiParam("Code of the endpoint") String code);
 
 	/**
 	 * Generates and returns the request schema of a given endpoint.
@@ -85,7 +109,10 @@ public interface IEndpointRs {
 	 * @param code code of the endpoint
 	 * @return request schema of the given endpoint
 	 */
-	String requestSchema(String code);
+	@GET
+	@Path("/schema/{code}/request")
+	@ApiOperation(value = "Generates and returns the request schema of a given endpoint.")
+	String requestSchema(@PathParam("code") @NotNull @ApiParam("Code of the endpoint") String code);
 
 	/**
 	 * Generates and returns the response schema of a given endpoint.
@@ -93,6 +120,9 @@ public interface IEndpointRs {
 	 * @param code code of the endpoint
 	 * @return response schema of the given endpoint
 	 */
-	String responseSchema(String code);
+	@GET
+	@Path("/schema/{code}/response")
+	@ApiOperation(value = "Generates and returns the response schema of a given endpoint.")
+	String responseSchema(@PathParam("code") @NotNull @ApiParam("Code of the endpoint") String code);
 
 }
