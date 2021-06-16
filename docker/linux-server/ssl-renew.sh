b25261cabf017fcb3e5239339920815e7b467957
anaxdev <anaxdev@outlook.com>
2021-06-16 14:38:15 +0700

    Update some scripts for the deployment

--- docker/linux-server/ssl-renew.sh
+++ docker/linux-server/ssl-renew.sh
@@ -2,7 +2,7 @@
 
 # DOMAIN_NAME and SERVER_NAME will be replaced by the correct value in install.sh
 
-docker stop {{STACK_NAME}}-nginx
+docker-compose stop nginx
 
 certbot renew
 kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
@@ -10,4 +10,4 @@ kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
 cp /etc/letsencrypt/live/DOMAIN_NAME/fullchain.pem  /home/SERVER_NAME/conf/ssl/domain.crt
 cp /etc/letsencrypt/live/DOMAIN_NAME/privkey.pem  /home/SERVER_NAME/conf/ssl/domain.key
 
-docker start {{STACK_NAME}}-nginx
+docker-compose up -d nginx
