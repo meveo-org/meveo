4f410317334a8a5b98e41d21a156c84045328155
Clément Bareth-Desgranges <clement.bareth@gmail.com>
2021-09-03 16:43:41 +0200

    Revert "CrossStorageDataModel - don't filter fields to fetch on identifer"
    This reverts commit 1446e18a5ce26132cbea141ef69ab8e3f124162c.

--- meveo-admin/web/src/main/java/org/meveo/util/view/CrossStorageDataModel.java
+++ meveo-admin/web/src/main/java/org/meveo/util/view/CrossStorageDataModel.java
@@ -66,6 +66,7 @@ public abstract class CrossStorageDataModel extends LazyDataModel<Map<String, Ob
 		List<String> summaryFields = cftService.findByAppliesTo(getCustomEntityTemplate().getAppliesTo())
 				.values()
 				.stream()
+				.filter(CustomFieldTemplate::isSummary)
 				.filter(Predicate.not(CustomFieldTemplate::isIdentifier))
 				.sorted((field1, field2) -> field1.getGUIFieldPosition() - field2.getGUIFieldPosition())
 				.map(CustomFieldTemplate::getCode)
