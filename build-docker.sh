3988d51c8ac1dfb7490d683b58cf1841be08b231
anaxdev <anaxdev@outlook.com>
2021-09-09 18:45:58 +0700

    Fixed dockerignore, docker buildkit

--- build-docker.sh
+++ build-docker.sh
@@ -11,16 +11,11 @@ info "------------------------------------------------"
 info "          Building meveo docker image           "
 info "------------------------------------------------"
 
-# Enable docker buildkit
-DOCKER_BUILDKIT=1
+# Enable docker buildkit,
 # Increase the building log limit to 50MB. (Default limit is 1MB)
-BUILDKIT_STEP_LOG_MAX_SIZE=50000000
-# Build docker image
-docker build -t ${DCK_IMAGE_REPO}/wildfly-meveo:${DCK_IMAGE_TAG} .
+DOCKER_BUILDKIT=1 BUILDKIT_STEP_LOG_MAX_SIZE=50000000 \
+  docker build -t ${DCK_IMAGE_REPO}/wildfly-meveo:${DCK_IMAGE_TAG} .
 
 info "------------------------------------------------"
 info "          Meveo docker - BUILD SUCCESS          "
 info "------------------------------------------------"
-
-info "Remove the useless image"
-docker system prune -f
