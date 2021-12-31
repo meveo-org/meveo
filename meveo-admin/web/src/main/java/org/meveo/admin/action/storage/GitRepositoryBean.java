ea99e28d14a4b236583361d198bbc5f20e6f8328
Sebastien Michea <smichea@gmail.com>
2021-12-31 04:29:06 +0100

    test #476

--- meveo-admin/web/src/main/java/org/meveo/admin/action/storage/GitRepositoryBean.java
+++ meveo-admin/web/src/main/java/org/meveo/admin/action/storage/GitRepositoryBean.java
@@ -183,9 +183,11 @@ public class GitRepositoryBean extends BaseCrudBean<GitRepository, GitRepository
 	}
 	
 	public boolean isModuleRepository() {
-		File gitDir = GitHelper.getRepositoryDir(currentUser, entity.getCode());
-		if (gitDir.exists() && gitDir.isDirectory()) {
-			return Arrays.stream(gitDir.list()).anyMatch("module.json"::equals);
+		if(entity.getCode()!=null){
+			File gitDir = GitHelper.getRepositoryDir(currentUser, entity.getCode());
+			if (gitDir.exists() && gitDir.isDirectory()) {
+				return Arrays.stream(gitDir.list()).anyMatch("module.json"::equals);
+			}
 		}
 		return false;
 	}
