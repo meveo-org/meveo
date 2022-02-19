2563d76ddbfcd9a86e38f2eb4a4ba01a6af1be17
masumcse1 <optinfra.masum@gmail.com>
2022-02-19 14:11:40 +0600

    fix #437

--- meveo-api/src/main/java/org/meveo/api/PermissionApi.java
+++ meveo-api/src/main/java/org/meveo/api/PermissionApi.java
@@ -5,12 +5,14 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.api.dto.PermissionDto;
 import org.meveo.api.dto.PermissionsDto;
+import org.meveo.api.exception.ActionForbiddenException;
+import org.meveo.api.exception.EntityAlreadyExistsException;
 import org.meveo.api.exception.EntityDoesNotExistsException;
 import org.meveo.api.exception.MeveoApiException;
-import org.meveo.api.exception.MissingParameterException;
 import org.meveo.model.security.Permission;
 import org.meveo.service.admin.impl.PermissionService;
 import org.meveo.service.base.local.IPersistenceService;
@@ -40,45 +42,137 @@ public class PermissionApi extends BaseCrudApi<Permission, PermissionDto> {
 	}
 
 	@Override
-	public PermissionDto find(String code) throws EntityDoesNotExistsException, MissingParameterException
-	{
-		/*if (StringUtils.isBlank(name)) {
-	        missingParameters.add("roleName");
-	    }
-	
-	    handleMissingParameters();
-	
-	    if (!(currentUser.hasRole("superAdminManagement") || (currentUser.hasRole("administrationVisualization")))) {
-	        throw new ActionForbiddenException("User has no permission to access roles");
-	    }
-	
-	    RoleDto roleDto = null;
-	    Role role = roleService.findByName(name);
-	    if (role == null) {
-	        throw new EntityDoesNotExistsException(Role.class, name, "name");
-	    }
-	    roleDto = new RoleDto(role, true, true);
-	
-	    return roleDto;*/
-	return null;
+	public PermissionDto find(String permission) throws MeveoApiException {
+
+		if (StringUtils.isBlank(permission)) {
+			missingParameters.add("permission");
+		}
+
+		handleMissingParameters();
+
+		if (!(currentUser.hasRole("superAdminManagement") || (currentUser.hasRole("administrationVisualization")))) {
+			throw new ActionForbiddenException("User has no permission to access roles");
+		}
+
+		PermissionDto permissionDto = null;
+		Permission permissionEntity = permissionService.findByPermission(permission);
+		if (permission == null) {
+			throw new EntityDoesNotExistsException(Permission.class, permission, "permission");
+		}
+		permissionDto = new PermissionDto(permissionEntity);
+
+		return permissionDto;
+
 	}
 
 	@Override
 	public Permission createOrUpdate(PermissionDto dtoData) throws MeveoApiException, BusinessException {
-		/*
-		 * String name = postData.getName(); if (name == null) {
-		 * missingParameters.add("name"); }
-		 * 
-		 * handleMissingParameters();
-		 * 
-		 * if (!(currentUser.hasRole("superAdminManagement") ||
-		 * (currentUser.hasRole("administrationManagement")))) { throw new
-		 * ActionForbiddenException("User has no permission to manage roles"); }
-		 * 
-		 * Role role = roleService.findByName(name); if (role == null) { return
-		 * create(postData); } else { return update(postData); }
-		 */
-		return null;
+		
+		String name = dtoData.getName();
+		if (StringUtils.isBlank(name)) {
+			missingParameters.add("name");
+		}
+
+		String permissionValue = dtoData.getPermission();
+		if (permissionValue == null) {
+			missingParameters.add("permission");
+		}
+		
+		handleMissingParameters();
+
+		if (!(currentUser.hasRole("superAdminManagement") || (currentUser.hasRole("administrationManagement")))) {
+			throw new ActionForbiddenException("User has no permission to manage permissions.");
+		}
+
+		Permission permission = permissionService.findByPermission(permissionValue);
+
+		if (permission == null) {
+			return create(dtoData);
+		} else {
+			return update(dtoData);
+		}
+	}
+
+	/**
+	 *
+	 * @param postData posted data to API
+	 *
+	 * @return Permission entity
+	 * @throws MeveoApiException meveo api exception
+	 * @throws BusinessException meveo api exception
+	 */
+	public Permission create(PermissionDto postData) throws MeveoApiException, BusinessException {
+
+		String name = postData.getName();
+		if (StringUtils.isBlank(name)) {
+			missingParameters.add("name");
+		}
+
+		if (StringUtils.isBlank(postData.getPermission())) {
+			missingParameters.add("permission");
+		}
+
+		handleMissingParameters();
+
+		if (permissionService.findByPermission(postData.getPermission()) != null) {
+			throw new EntityAlreadyExistsException(Permission.class, name, "name");
+		}
+
+		if (!(currentUser.hasRole("superAdminManagement") || (currentUser.hasRole("administrationManagement")))) {
+			throw new ActionForbiddenException("User has no permission to manage permissions.");
+		}
+
+		Permission permission = new Permission();
+		permission.setName(postData.getName());
+		permission.setPermission(postData.getPermission());
+		
+		permissionService.create(permission);
+
+		return permission;
+	}
+
+	/**
+	 * Update permission.
+	 *
+	 * @param postData Permission DTO
+	 *
+	 * @return Updated Permission entity
+	 * @throws MeveoApiException meveo api exception
+	 * @throws BusinessException business exception.
+	 */
+	public Permission update(PermissionDto postData) throws MeveoApiException, BusinessException {
+
+		String name = postData.getName();
+		if (StringUtils.isBlank(name)) {
+			missingParameters.add("name");
+		}
+
+	
+		if (StringUtils.isBlank(postData.getPermission())) {
+			missingParameters.add("permission");
+		}
+		
+		handleMissingParameters();
+
+		if (!(currentUser.hasRole("superAdminManagement") || (currentUser.hasRole("administrationManagement")))) {
+			throw new ActionForbiddenException("User has no permission to manage permission.");
+		}
+
+		Permission permission = permissionService.findByPermission(postData.getPermission());
+
+		if (permission == null) {
+			throw new EntityDoesNotExistsException(Permission.class, postData.getPermission(), "permission");
+		}
+
+		if (postData.getName() != null) {
+			permission.setName(postData.getName());
+		}
+
+		if (postData.getPermission() != null) {
+			permission.setPermission(postData.getPermission());
+		}
+
+		return permissionService.update(permission);
 	}
 
 	@Override
