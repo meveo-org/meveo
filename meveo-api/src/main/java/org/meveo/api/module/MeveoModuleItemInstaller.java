add718b80c5814d279706bb7321e9e3f82a630a6
Clément Bareth-Desgranges <clement.bareth@gmail.com>
2022-04-15 17:43:20 +0200

    #529 - Sort script on install

--- meveo-api/src/main/java/org/meveo/api/module/MeveoModuleItemInstaller.java
+++ meveo-api/src/main/java/org/meveo/api/module/MeveoModuleItemInstaller.java
@@ -41,6 +41,7 @@ import org.meveo.api.exception.ActionForbiddenException;
 import org.meveo.api.exception.EntityAlreadyExistsException;
 import org.meveo.api.exception.MeveoApiException;
 import org.meveo.api.exceptions.ModuleInstallFail;
+import org.meveo.commons.utils.MvCollectionUtils;
 import org.meveo.commons.utils.ReflectionUtils;
 import org.meveo.commons.utils.StringUtils;
 import org.meveo.model.BusinessEntity;
@@ -689,8 +690,8 @@ public class MeveoModuleItemInstaller {
 			}
 		};
 
-		bubbleSort(sortedItems, comparator);
-		//sortedItems.sort(comparator);
+		MvCollectionUtils.bubbleSort(sortedItems, comparator);
+		
 		return sortedItems;
 	}
 	
@@ -791,49 +792,6 @@ public class MeveoModuleItemInstaller {
     	
     }
     
-	private static <T extends Comparable> void bubbleSort(List<T> objects, Comparator<T> comparator) {
-
-		/*
-		 * In bubble sort, we basically traverse the array from first to array_length - 1 position and compare the element with the next one. Element is
-		 * swapped with the next element if the next element is smaller.
-		 * This sort assume that comparable.compare() returns 0 when objects are not comparable
-		 * 
-		 */
-
-		int n = objects.size();
-
-		for (int i = 0; i < n; i++) {
-			boolean swapped = false;
-			for (int j = 1; j < (n - i); j++) {
-
-				int compareJMinus1AndJ = comparator.compare(objects.get(j - 1), objects.get(j));
-				if (compareJMinus1AndJ>0) {
-					// swap the elements!
-					T temp = objects.get(j - 1);
-					objects.set(j - 1, objects.get(j));
-					objects.set(j, temp);
-					swapped = true;
-				} else if (compareJMinus1AndJ==0 & j>1) {
-					for (int z = j-2; z>=0; z--) {
-						int compareZAndJ = comparator.compare(objects.get(j - z), objects.get(j));
-						if (compareZAndJ>0) {
-							T temp = objects.get(z);
-							objects.set(z, objects.get(j));
-							objects.set(j, temp);
-							swapped = true;
-						}else if (compareZAndJ<0) {
-							break;
-						}
-					}
-				}
-
-			}
-			if (!swapped)
-				break;
-		}
-
-	}
-
 	
 	/**
 	 * Add cft which is a field of cet as a module item.
