27a5d44736864834ccdd9b32be64d3e988572c1b
fmunir1153 <farhan.munir@manaty.net>
2022-06-23 00:21:17 +0500

    https://github.com/meveo-org/meveo/issues/565

--- meveo-admin/ejbs/src/test/java/org/meveo/service/technicalservice/endpoint/EndpointRetrieval.java
+++ meveo-admin/ejbs/src/test/java/org/meveo/service/technicalservice/endpoint/EndpointRetrieval.java
@@ -6,6 +6,7 @@ import org.meveo.model.technicalservice.endpoint.EndpointHttpMethod;
 import org.meveo.model.technicalservice.endpoint.EndpointParameter;
 import org.meveo.model.technicalservice.endpoint.EndpointPathParameter;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class EndpointRetrieval {
@@ -26,6 +27,35 @@ public class EndpointRetrieval {
         endpointPathParameter2.setEndpointParameter(second);
         second.setParameter("second");
         endpoint.addPathParameter(endpointPathParameter2);
+        cache.addEndpoint(endpoint);
+
+        endpoint = new Endpoint();
+        endpoint.setCode("country");
+        endpoint.setBasePath("country");
+        endpoint.setMethod(EndpointHttpMethod.GET);
+        endpointPathParameter = new EndpointPathParameter();
+        first = new EndpointParameter();
+        endpointPathParameter.setEndpointParameter(first);
+        first.setParameter("first");
+        endpoint.addPathParameter(endpointPathParameter);
+
+        cache.addEndpoint(endpoint);
+
+        endpoint = new Endpoint();
+        endpoint.setCode("country-by-status");
+        endpoint.setBasePath("country-by-status");
+        endpoint.setMethod(EndpointHttpMethod.GET);
+        endpointPathParameter = new EndpointPathParameter();
+        first = new EndpointParameter();
+        endpointPathParameter.setEndpointParameter(first);
+        first.setParameter("first");
+        endpoint.addPathParameter(endpointPathParameter);
+        endpointPathParameter2 = new EndpointPathParameter();
+        second = new EndpointParameter();
+        endpointPathParameter2.setEndpointParameter(second);
+        second.setParameter("second");
+        endpoint.addPathParameter(endpointPathParameter2);
+        endpoint.setPath("/{first}/id/{second}");
         cache.addEndpoint(endpoint);
 
         endpoint = new Endpoint();
@@ -78,19 +108,41 @@ public class EndpointRetrieval {
         endpoint.setBasePath("idservice");
         endpoint.setPath("/{first}/manifest/{second}");
         cache.addEndpoint(endpoint);
+
+        endpoint = new Endpoint();
+        endpoint.setCode("city");
+        endpoint.setBasePath("city");
+        endpoint.setMethod(EndpointHttpMethod.GET);
+
+        cache.addEndpoint(endpoint);
     }
 
     @Test
     public void retrieveEndpoint(){
+
         Endpoint endpoint = cache.getEndpointForPath("/mytest/param1/param2?param3=1","GET");
         assert("mytest".equals(endpoint.getCode()));
+
         endpoint = cache.getEndpointForPath("/mytest2/param1/param2?param3=1","GET");
         assertNull(endpoint);
+
         endpoint = cache.getEndpointForPath("/idservice/param1/id/param2?param3=1","GET");
         assert("idservice".equals(endpoint.getCode()));
+
         endpoint = cache.getEndpointForPath("/idservice/param1/manifest/param2?param3=1","GET");
         assert("manifestservice".equals(endpoint.getCode()));
+
         endpoint = cache.getEndpointForPath("/idservice/param1/manifest/param2?param3=1","POST");
         assert("manifestservicePost".equals(endpoint.getCode()));
+
+        endpoint = cache.getEndpointForPath("/country-by-status/param1/id/param2?param3=1","GET");
+        assert("country-by-status".equals(endpoint.getCode()));
+
+        endpoint = cache.getEndpointForPath("/country-by-name/param1/param2?param3=1","GET");
+        assertNull(endpoint);
+
+        endpoint = cache.getEndpointForPath("/country-by-status/param1","GET");
+        assertNull(endpoint);
+
     }
 }
