2d08c58ed75c4bc57d9fa733043e1cdbc9ac6f64
Clément Bareth-Desgranges <clement.bareth@gmail.com>
2022-08-20 22:41:00 +0200

    #590 - API side & send message

--- meveo-admin-ejbs/src/main/java/org/meveo/service/technicalservice/wsendpoint/WebsocketClientService.java
+++ meveo-admin-ejbs/src/main/java/org/meveo/service/technicalservice/wsendpoint/WebsocketClientService.java
@@ -15,112 +15,13 @@
  */
 package org.meveo.service.technicalservice.wsendpoint;
 
-import java.io.File;
-import java.util.List;
-
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Root;
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.Context;
 
-import org.meveo.admin.exception.BusinessException;
-import org.meveo.keycloak.client.KeycloakAdminClientService;
-import org.meveo.model.git.GitRepository;
-import org.meveo.model.scripts.Function;
-import org.meveo.model.technicalservice.wsendpoint.WSEndpoint;
 import org.meveo.model.technicalservice.wsendpoint.WebsocketClient;
 import org.meveo.service.base.BusinessService;
-import org.meveo.service.git.GitHelper;
-import org.meveo.service.git.MeveoRepository;
 
-/**
- * EJB for managing technical services websocket endpoints
- *
- * @since 01.02.2019
- * @version 6.9.0
- */
 @Stateless
 public class WebsocketClientService extends BusinessService<WebsocketClient> {
 
-	public static final String EXECUTE_ALL_ENDPOINTS = "Execute_All_Endpoints";
-	public static final String ENDPOINTS_CLIENT = "endpoints";
-	public static final String EXECUTE_ENDPOINT_TEMPLATE = "Execute_Endpoint_%s";
-	public static final String ENDPOINT_MANAGEMENT = "endpointManagement";
-
-	@Context
-	private HttpServletRequest request;
-
-	@EJB
-	private KeycloakAdminClientService keycloakAdminClientService;
-
-	@Inject
-	@MeveoRepository
-	private GitRepository meveoRepository;
-
-	public static String getEndpointPermission(WSEndpoint endpoint) {
-		return String.format(EXECUTE_ENDPOINT_TEMPLATE, endpoint.getCode());
-	}
-
-	/**
-	 * Retrieve all endpoints associated to the given service
-	 *
-	 * @param code Code of the service
-	 * @return All endpoints associated to the service with the provided code
-	 */
-	public List<WSEndpoint> findByServiceCode(String code) {
-		CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-		CriteriaQuery<WSEndpoint> query = cb.createQuery(WSEndpoint.class);
-		Root<WSEndpoint> root = query.from(WSEndpoint.class);
-		final Join<WSEndpoint, Function> service = root.join("service");
-		query.where(cb.equal(service.get("code"), code));
-		return getEntityManager().createQuery(query).getResultList();
-	}
-
 	
-	/**
-	 * Checks if an endpoint interface is already created and pushed in git
-	 * repository.
-	 * 
-	 * @param endpoint endpoint to search
-	 * @return true if endpoint interface exists
-	 */
-	public boolean isEndpointScriptExists(WSEndpoint endpoint) {
-		final File repositoryDir = GitHelper.getRepositoryDir(currentUser, meveoRepository.getCode());
-		final File endpointDir = new File(repositoryDir, "/wsendpoints/" + endpoint.getCode());
-		File f = new File(endpointDir, endpoint.getCode() + ".js");
-
-		return f.exists() && !f.isDirectory();
-	}
-
-	/**
-	 * Checks if the base endpoint interface is already created and pushed in git
-	 * repository.
-	 * 
-	 * @param endpoint endpoint to search
-	 * @return true if endpoint interface exists
-	 */
-	public boolean isBaseEndpointScriptExists() {
-		final File repositoryDir = GitHelper.getRepositoryDir(currentUser, meveoRepository.getCode());
-		final File f = new File(repositoryDir, "/wsendpoints/" + WSEndpoint.ENDPOINT_INTERFACE_JS + ".js");
-
-		return f.exists() && !f.isDirectory();
-	}
-
-	public File getScriptFile(WSEndpoint endpoint) {
-		final File repositoryDir = GitHelper.getRepositoryDir(currentUser, meveoRepository.getCode());
-		final File endpointDir = new File(repositoryDir, "/wsendpoints/" + endpoint.getCode());
-		endpointDir.mkdirs();
-		return new File(endpointDir, endpoint.getCode() + ".js");
-	}
-	
-	public File getBaseScriptFile() {
-		final File repositoryDir = GitHelper.getRepositoryDir(currentUser, meveoRepository.getCode());
-		final File endpointFile = new File(repositoryDir, "/wsendpoints/" + WSEndpoint.ENDPOINT_INTERFACE_JS + ".js");
-		return endpointFile;
-	}
 }
--- meveo-admin-ejbs/src/main/java/org/meveo/service/technicalservice/wsendpoint/WebsocketManager.java
+++ meveo-admin-ejbs/src/main/java/org/meveo/service/technicalservice/wsendpoint/WebsocketManager.java
@@ -4,6 +4,7 @@
 package org.meveo.service.technicalservice.wsendpoint;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -13,9 +14,13 @@ import javax.enterprise.context.Destroyed;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.websocket.Session;
 
 import org.meveo.admin.exception.BusinessException;
-import org.meveo.cache.UserMessageCacheContainerProvider;
+import org.meveo.event.qualifier.Created;
+import org.meveo.event.qualifier.Removed;
+import org.meveo.event.qualifier.Updated;
 import org.meveo.model.technicalservice.wsendpoint.WebsocketClient;
 import org.meveo.service.script.ConcreteFunctionService;
 import org.meveo.service.script.ScriptInterface;
@@ -38,28 +43,87 @@ public class WebsocketManager {
 	
 	private final Map<String, WebsocketClientInstance> websocketClients = new ConcurrentHashMap<>();
 	
+	@Transactional
     public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
     	for (WebsocketClient client : wsEndpointService.listActive()) {
-    		// Init function
-    		ScriptInterface function;
     		try {
-    			function = concreteFunctionService.getExecutionEngine(client.getService(), new HashMap<>());
-			} catch (BusinessException e) {
-				log.error("Failed to init function", e);
-				continue;
-			}
-    		
-    		// Connect to server (async process)
-    		CompletableFuture.runAsync(() -> {
-    			WebsocketClientInstance instance = new WebsocketClientInstance(client, function, websocketExecutionService);
-    			try {
-    				instance.connect();
-    				websocketClients.put(client.getCode(), instance);
-    			} catch (Exception e) {
-    				log.warn("Failed to connect to websocket {}", client.getCode(), e);
-    			}
-    		});
+    			initWebSocketClient(client);
+    		} catch (BusinessException e) {
+    			log.error("Failed to init function", e);
+    			continue;
+    		}
     	}
+    }
+    
+    public void closeClient(@Observes @Removed WebsocketClient client) throws Exception {
+    	var clientInstance = websocketClients.remove(client.getCode());
+    	if (clientInstance != null) {
+    		clientInstance.close();
+    	}
+    }
+    
+    @Transactional
+    public boolean connect(WebsocketClient client, ScriptInterface function) {
+    	WebsocketClientInstance instance = websocketClients.get(client.getCode());
+    	if (instance != null && instance.isConnected()) {
+    		return true;
+    	}
+    	
+		try {
+			if (function == null) function = concreteFunctionService.getExecutionEngine(client.getService(), new HashMap<>());
+	    	instance = new WebsocketClientInstance(client, function, websocketExecutionService);
+			instance.connect();
+			websocketClients.put(client.getCode(), instance);
+			return instance.isConnected();
+		} catch (Exception e) {
+			log.warn("Failed to connect to websocket {}", client.getCode(), e);
+			return false;
+		}
+    }
+    
+	public void sendMessage(String websocketCode, String username, String txtMessage, boolean persistMessage) {
+		String persistCacheKey = websocketCode+"_"+username;
+		sendMessage(websocketCode, username, txtMessage, persistCacheKey, persistMessage);
+	}
+	
+	public void sendMessage(String websocketCode, String username, String txtMessage, String persistCacheKey, boolean persistMessage) {
+		boolean messageSent = false;
+
+		var instance = websocketClients.get(websocketCode);
+		if (instance != null) {
+			if (!instance.isConnected()) {
+				//TODO: Auto-reconnect
+//				connect(null, null);
+//				instance = websocketClients.get(websocketCode);
+			}
+			var session = instance.getSession();
+			if (session.isOpen()) {
+				session.getAsyncRemote().sendText(txtMessage);
+				messageSent = true;
+			}
+		} else {
+			// TODO use WebsockerServerEndpoint
+		}
+	}
+    
+    @Transactional
+    public void initWebSocketClient(@Observes @Created WebsocketClient client) throws BusinessException {
+    	if (!client.isActive()) {
+    		return;
+    	}
+    	
+    	var function = concreteFunctionService.getExecutionEngine(client.getService(), new HashMap<>());
+    	
+		// Connect to server (async process)
+		CompletableFuture.runAsync(() -> {
+			connect(client, function);
+		});
+    }
+    
+    @Transactional
+    public void resetClient(@Observes @Updated WebsocketClient client) throws Exception {
+    	closeClient(client);
+    	initWebSocketClient(client);
     }
     
     public void destroy(@Observes @Destroyed(ApplicationScoped.class) Object init) {
