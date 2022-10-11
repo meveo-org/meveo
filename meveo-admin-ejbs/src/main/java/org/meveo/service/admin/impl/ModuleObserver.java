e9bee475ebb62e895dfc8675e323444d86610c03
Clément Bareth-Desgranges <clement.bareth@gmail.com>
2022-10-11 14:35:18 +0200

    #617 - Remove observer on module item delete

--- meveo-admin-ejbs/src/main/java/org/meveo/service/admin/impl/ModuleObserver.java
+++ meveo-admin-ejbs/src/main/java/org/meveo/service/admin/impl/ModuleObserver.java
@@ -91,38 +91,4 @@ public class ModuleObserver {
 		}
 	}
 		
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Transactional(value = TxType.REQUIRES_NEW)
-	public void onItemDelete(@Observes(during = TransactionPhase.AFTER_SUCCESS) @PostRemoved MeveoModuleItem item, BusinessServiceFinder bsf, MeveoModuleService moduleService) {
-		if (item.getMeveoModule() == null) {
-			return;
-		}
-		
-		MeveoModule meveoModule = moduleService.findById(item.getMeveoModule().getId());
-		// Module has been deleted, so as the git repository and it's items so we don't need to remove them
-		if (meveoModule == null) {
-			return;
-		}
-		
-		try {
-			moduleService.loadModuleItem(item);
-		} catch (BusinessException e1) {
-			log.info("Can't load entity for item {}", e1);
-		}
-		
-		BusinessEntity entity = item.getItemEntity();
-		
-		//FIXME: Don't do that on module uninstallation
-		if (entity != null) {
-			MeveoModule mainModule = moduleService.findByCode("Meveo");
-			BusinessService businessService = bsf.find(entity);
-			
-			try {
-				businessService.moveFilesToModule(entity, item.getMeveoModule(), mainModule);
-			} catch (BusinessException | IOException e) {
-				log.info("Fail to move files to main module", e);
-			}
-		}
-
-	}
 }
