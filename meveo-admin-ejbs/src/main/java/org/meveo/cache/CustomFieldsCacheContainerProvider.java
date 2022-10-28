15e625bf670ec4955df12b608546db6287ef12c4
Clément Bareth-Desgranges <clement.bareth@gmail.com>
2022-10-28 15:13:27 +0200

    Revert "#623 - Fix locking issues on getCustomFieldTemplates"
    This reverts commit 91b4e7988d370d7d04f6c68181e967c99156ba59.

--- meveo-admin-ejbs/src/main/java/org/meveo/cache/CustomFieldsCacheContainerProvider.java
+++ meveo-admin-ejbs/src/main/java/org/meveo/cache/CustomFieldsCacheContainerProvider.java
@@ -34,7 +34,6 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 import javax.annotation.PostConstruct;
 import javax.ejb.DependsOn;
 import javax.ejb.EJB;
-import javax.ejb.LockType;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
 import javax.naming.InitialContext;
@@ -522,7 +521,6 @@ public class CustomFieldsCacheContainerProvider implements Serializable {
      * @param appliesTo entity (appliesTo value)
      * @return A map of custom field templates with template code as a key or NULL if cache key not found
      */
-    @javax.ejb.Lock(LockType.READ)
     public Map<String, CustomFieldTemplate> getCustomFieldTemplates(String appliesTo) {
         CacheKeyStr key = new CacheKeyStr(null, appliesTo);
 
@@ -544,21 +542,13 @@ public class CustomFieldsCacheContainerProvider implements Serializable {
 //            lock.unlock();
         }
         if(cfMaps == null || cfMaps.isEmpty()) {
-        	Lock lock = cacheLock.writeLock();
-        	try {
-        		cfMaps = cftsByAppliesTo.get(key);
-        		if(cfMaps == null || cfMaps.isEmpty()) {
-	        		cfMaps = customFieldTemplateService.findByAppliesToNoCache(appliesTo);
-	        		if(cfMaps != null){
-	        			cfMaps.forEach((k,v) -> {
-	        				customFieldTemplateService.detach(v);
-	        				addUpdateCustomFieldTemplate(v);
-	        			});
-	        		}
-        		}
-        	} finally {
-        		lock.unlock();
-        	}
+            cfMaps = customFieldTemplateService.findByAppliesToNoCache(appliesTo);
+            if(cfMaps != null){
+                cfMaps.forEach((k,v) -> {
+                    customFieldTemplateService.detach(v);
+                    addUpdateCustomFieldTemplate(v);
+                });
+            }
         }
         return cfMaps;
     }
