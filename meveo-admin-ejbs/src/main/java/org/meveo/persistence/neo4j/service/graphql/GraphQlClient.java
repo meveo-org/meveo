ed5a72f40c7f949a7aa67e027f10c031db34b750
Clément Bareth-Desgranges <clement.bareth@gmail.com>
2022-11-03 16:15:16 +0100

    #606 - Handle auth

--- meveo-admin-ejbs/src/main/java/org/meveo/persistence/neo4j/service/graphql/GraphQlClient.java
+++ meveo-admin-ejbs/src/main/java/org/meveo/persistence/neo4j/service/graphql/GraphQlClient.java
@@ -7,7 +7,9 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
+import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
@@ -16,6 +18,8 @@ import javax.ws.rs.core.Response.Status.Family;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.meveo.model.neo4j.GraphQLRequest;
+import org.meveo.security.CurrentUser;
+import org.meveo.security.MeveoUser;
 
 /**
  * A java client for the meveo neo4j-graphql server
@@ -28,6 +32,10 @@ import org.meveo.model.neo4j.GraphQLRequest;
  */
 public class GraphQlClient {
 	
+	@Inject
+	@CurrentUser	
+	private MeveoUser currentUser;
+	
 	private Client client;
 
 	@PostConstruct
@@ -37,16 +45,19 @@ public class GraphQlClient {
 	
 	@SuppressWarnings("unchecked")
 	public Map<String, Object> executeGraphQlRequest(GraphQLRequest graphQLRequest, String graphqlEndpoint) {
-		
 		try {
 			return client.target(graphqlEndpoint)
 					.path("/graphql")
 					.request()
+					.header("Authorization", "Bearer " + currentUser.getToken())
 					.buildPost(Entity.entity(graphQLRequest, MediaType.APPLICATION_JSON))
 					.invoke(Map.class);
 		} catch (BadRequestException e) {
 			String errorMessage = e.getResponse().readEntity(String.class);
 			throw new IllegalArgumentException(errorMessage);
+		} catch (ServerErrorException e) {
+			String errorMessage = e.getResponse().readEntity(String.class);
+			throw new RuntimeException(errorMessage);
 		}
 
 	}
@@ -56,6 +67,7 @@ public class GraphQlClient {
 			var response = client.target(graphqlEndpoint)
 				.path("/update")
 				.request()
+				.header("Authorization", "Bearer " + currentUser.getToken())
 				.buildPost(Entity.entity(idl, MediaType.TEXT_PLAIN))
 				.invoke();
 			
