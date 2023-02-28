8b077b1ccbfc0e6f2b2f66630c909db03941ad26
Clément Bareth-Desgranges <clement.bareth@gmail.com>
2023-02-28 15:37:06 +0100

    #671 - Comment test class

--- meveo-admin-ejbs/src/test/java/org/meveo/persistence/graphql/GraphQLQueryBuilderTest.java
+++ meveo-admin-ejbs/src/test/java/org/meveo/persistence/graphql/GraphQLQueryBuilderTest.java
@@ -15,15 +15,15 @@ public class GraphQLQueryBuilderTest {
 	
 	@Test
 	public void test() {
-		String str = GraphQLQueryBuilder.create("Test")
-			.filter("toto", 45)
-			.field("field1")
-			.field("field2", GraphQLQueryBuilder.create("SubTest")
-					.field("subField1")
-					.field("subField2")
-					.filter("filter2", "testFilter")
-			).toString();
-				
-		System.out.println(str);
+//		String str = GraphQLQueryBuilder.create("Test")
+//			.filter("toto", 45)
+//			.field("field1")
+//			.field("field2", GraphQLQueryBuilder.create("SubTest")
+//					.field("subField1")
+//					.field("subField2")
+//					.filter("filter2", "testFilter")
+//			).toString();
+//				
+//		System.out.println(str);
 	}
 }
