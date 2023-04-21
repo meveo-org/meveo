991e39319f906261d30ac0fb1ac2df23ffdcaa0f
Clément Bareth-Desgranges <clement.bareth@gmail.com>
2023-04-21 10:30:24 +0200

    Remove useless imports

--- meveo-admin-ejbs/src/main/java/org/meveo/persistence/graphql/GraphQLQueryBuilder.java
+++ meveo-admin-ejbs/src/main/java/org/meveo/persistence/graphql/GraphQLQueryBuilder.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.atteo.evo.inflector.English;
 import org.meveo.commons.utils.CamelCaseUtils;
 import org.meveo.model.neo4j.Neo4JConfiguration;
 import org.meveo.util.Inflector;
